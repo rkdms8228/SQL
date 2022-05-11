4dd26ba0fa270f0bbfbebeb66839c5f08fbf9de0
가은 <rkdms8228@naver.com>
2022-05-11 16:06:40 +0900

    2022-05-11 수업 내용 추가

--- Ex/Ex01.sql
+++ Ex/Ex01.sql
@@ -4,3 +4,6 @@ select *
 from employees; /*줄 바꿈을 해도 이상 없음 | 마침표를 기준으로 인식*/
 
 select * from departments;
+
+
+select employee_id, first_name, phone_number from employees; /*컴마(,)를 기점으로 나눔 | 앞에 오는 순서부터 결과를 보여 줌*/

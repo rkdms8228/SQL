36979124f697dc7659664aa45a79fda354b48f3a
가은 <rkdms8228@naver.com>
2022-05-16 16:35:14 +0900

    SubQuery

---
+++ Ex/Ex04.sql
@@ -0,0 +1,43 @@
+--=========================--
+/*  SubQuery */
+
+--‘Den’보다 급여가 높은 사람의 이름과 급여는?
+select first_name
+          , salary
+from employees
+where salary > (select salary
+                        from employees
+                        where first_name = 'Den'); --SubQuery
+
+--연산자 : = , > , >=, < , <=, <>(같지 않다)
+
+-----------------------------------------------------------------
+
+--급여를 가장 적게 받는 사람의 이름, 급여, 사원 번호는?
+select first_name
+          , salary
+          , employee_id
+from employees
+where salary <= (select min(salary)
+                          from employees);
+
+--평균 급여보다 적게 받는 사람의 이름, 급여를 출력하세요.
+select first_name
+          , salary
+          , employee_id
+from employees
+where salary < (select avg(salary)
+                        from employees);
+                        
+--부서 번호가 110인 직원의 급여와 같은 모든 직원의 사번, 이름, 급여를 출력하세요.
+select first_name
+          , salary
+          , employee_id
+from employees
+where salary in (select salary
+                          from employees
+                          where department_id = 110);
+
+-----------------------------------------------------------------
+
+
